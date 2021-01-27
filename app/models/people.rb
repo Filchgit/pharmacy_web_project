104ba51422f50d4d1244d39a77ff779f1642d79e
John MacAulay <merjohn@bigpond.net.au>
2021-01-28 09:36:45 +1100

    started my own little project

---
+++ app/models/people.rb
@@ -0,0 +1,12 @@
+class People
+  def initiallize (attributes = {})
+    first_name = attributes[:first_name]
+    last_name = attributes[:last_name]
+    address_first_line = [:address_first_line]
+    address_second_line = [:address_second_line]
+    suburb = [:suburb]
+    postcode = [:postcode]
+    phone_number = [:phone_number]
+    email = [:email]
+     
+end
